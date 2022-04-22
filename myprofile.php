cd53a62fea555d7a2340c1196ce48d3bcdc13fd7
thotho295 <nguyenthuthao2905@gmail.com>
2022-04-22 09:26:12 +0700

    test

--- myprofile.php
+++ myprofile.php
@@ -56,14 +56,14 @@ if (strlen($_SESSION['emplogin']) == 0) {
                     -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
                     box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
                 }
-                .succWrap{
+                /* .succWrap{
                     padding: 10px;
                     margin: 0 0 20px 0;
                     background: #fff;
                     border-left: 4px solid #5cb85c;
                     -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
                     box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-                }
+                } */
             </style>
 
 
